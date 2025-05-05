845f4b09c26749c6f7a9de0885ff8dcd1fb5130e
shivyanshh <shivyansh2835@gmail.com>
2025-05-05 16:28:52 +0530

    minor update

--- logical_operators.java
+++ logical_operators.java
@@ -3,7 +3,7 @@ public class logical_operators {
         int x =2,y=5;
         //int y = 5;
         int a =5;
-        int b= 9;
+        
         boolean result = x > y || y==a ;
 
         System.out.println(result);
